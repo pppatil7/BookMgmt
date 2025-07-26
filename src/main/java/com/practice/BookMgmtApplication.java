30d34d9209585098972424f0c962d5843d99ff3c
pppatil7 <pundalikpatil7218@gmail.com>
2025-07-26 15:27:59 +0530

    first commit

---
+++ src/main/java/com/practice/BookMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMgmtApplication.class, args);
+	}
+
+}
