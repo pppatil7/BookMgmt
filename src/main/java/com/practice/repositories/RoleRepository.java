6f320d8c93498942f3c8a1ceb763ded2943e7083
pppatil7 <pundalikpatil7218@gmail.com>
2025-07-28 10:01:48 +0530

    role

---
+++ src/main/java/com/practice/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.practice.repositories;
+
+import com.practice.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
