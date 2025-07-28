ff39bcaf60ddc500db2b533ca284db9864263b2f
pppatil7 <pundalikpatil7218@gmail.com>
2025-07-28 08:57:15 +0530

    user

---
+++ src/main/java/com/practice/entities/User.java
@@ -0,0 +1,30 @@
+package com.practice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    private String userName;
+
+    private String userEmail;
+
+    private String userPassword;
+
+
+}
