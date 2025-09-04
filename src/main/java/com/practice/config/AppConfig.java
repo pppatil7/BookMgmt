07e336e2d2ef2fd326d95dcaf562948162eb6004
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-09-04 16:25:06 +0530

    userdetails service location changed

--- src/main/java/com/practice/config/AppConfig.java
+++ src/main/java/com/practice/config/AppConfig.java
@@ -1,17 +1,40 @@
 package com.practice.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
 
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//        UserDetails user1 = User.withUsername("admin")
+//                .password(passwordEncoder.encode("pass"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user2 = User.withUsername("student")
+//                .password(passwordEncoder.encode("pass"))
+//                .roles("STUDENT")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//
+//    }
 
 }
--- src/main/java/com/practice/config/WebSecurityConfig.java
+++ src/main/java/com/practice/config/WebSecurityConfig.java
@@ -16,8 +16,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private final PasswordEncoder passwordEncoder;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
@@ -30,23 +28,6 @@ public class WebSecurityConfig {
                 .formLogin(Customizer.withDefaults());
         return httpSecurity.build();
     }
-
-
-//    @Bean
-//    UserDetailsService userDetailsService() {
-//        UserDetails user1 = User.withUsername("admin")
-//                .password(passwordEncoder.encode("pass"))
-//                .roles("ADMIN")
-//                .build();
-//
-//        UserDetails user2 = User.withUsername("student")
-//                .password(passwordEncoder.encode("pass"))
-//                .roles("STUDENT")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user1, user2);
-//
-//    }
 
 
 }
