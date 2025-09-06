cca3ffddaeef2797e5d96ac178e4648416dec908
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-09-06 08:41:17 +0530

    exception handle

---
+++ src/main/java/com/practice/exceptions/ApiError.java
@@ -0,0 +1,28 @@
+package com.practice.exceptions;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class ApiError {
+
+    private LocalDateTime timeStamp;
+
+    private String error;
+
+    private HttpStatus statusCode;
+
+    public ApiError(LocalDateTime timeStamp) {
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public ApiError(String error, HttpStatus statusCode) {
+        this();
+        this.error = error;
+        this.statusCode = statusCode;
+    }
+}
